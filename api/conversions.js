63c97d8305dd0b8b36d3bae691ea4d63a46dd460
foolish66776 <136843313+foolish66776@users.noreply.github.com>
2025-07-31 17:19:35 +0200

    update conversions.js

--- api/conversions.js
+++ api/conversions.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    return res.status(200).json({ message: 'API Conversions endpoint attivo' });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Solo POST accettato' });
   }
-  res.status(405).json({ message: 'Solo POST accettato' });
+  res.status(200).json({ message: 'API Conversions endpoint attivo' });
 }
